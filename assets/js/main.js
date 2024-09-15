1a90c3e129e9599af5ad831699c03af756f896da
Kenneth Chapman <kenneth.chapman@gmail.com>
2024-09-15 13:08:49 -0400

    Added video upload and preview functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log('BJJ Roll Mapper is ready');
+document.getElementById('video-upload').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const videoPreview = document.getElementById('video-preview');
+    
+    if (file) {
+        const videoURL = URL.createObjectURL(file);
+        videoPreview.src = videoURL;
+        videoPreview.style.display = 'block';
+    } else {
+        videoPreview.src = '';
+        videoPreview.style.display = 'none';
+    }
+});
